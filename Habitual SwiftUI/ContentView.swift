a3624f3aebda8434557aff3b5d0a1c47797d1072
alaJream <jeremy.gonzales@students.dominican.edu>
2023-07-06 23:17:46 -0700

    Initial Commit

---
+++ Habitual SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habitual SwiftUI
+//
+//  Created by Jeremy Gonzales on 7/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habitual SwiftUI/Habitual_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habitual_SwiftUIApp.swift
+//  Habitual SwiftUI
+//
+//  Created by Jeremy Gonzales on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Habitual_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
